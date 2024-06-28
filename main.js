b57cb4ad4f0e9e3a89f25437830890f52c566643
Suleman <sulemangulzar07@gmail.com>
2024-06-28 23:32:06 +0500

    changed

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ var swiper = new Swiper(".mySwiper", {
   },
   keyboard: true,
 });
+
+function openNavbar() {
+  const navbar = document.getElementById("mobile-menu");
+  const rotateRight = document.getElementById("right-angle");
+  const currentLeft = navbar.style.left;
+
+  if (currentLeft === "0px") {
+    navbar.style.left = "-250px";
+    rotateRight.style.transform = "rotate(0deg)";
+  } else {
+    navbar.style.left = "0px";
+    rotateRight.style.transform = "rotate(180deg)";
+  }
+}
