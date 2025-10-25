c2eed14af48cc955ce679db2b99dd7b6558a1018
Suleman Gulzar <sulemangulzar>
2025-10-25 20:13:51 +0500

    Changed

--- portfolio/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from accounts.models import Contact
-
-@admin.register(Contact)
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone_number')
-    search_fields = ('name', 'email')
